c1547f94bee74a1e14881db728a6fe8aa15a11c4
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-05-28 21:13:19 +0800

    update MainActivity
    - Configure Button
    - Allign Verticaly and horizontally the column

--- app/src/main/java/com/thirdy/courserakotlin/MainActivity.kt
+++ app/src/main/java/com/thirdy/courserakotlin/MainActivity.kt
@@ -3,21 +3,31 @@ package com.thirdy.courserakotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.leanback.widget.Row
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            Column() {
+            Column(Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                 Text(
                     text = "Little Lemon", fontSize = 32.sp,
                     color= Color(0xFFF4CE14)
@@ -28,14 +38,18 @@ class MainActivity : ComponentActivity() {
                 )
 
                 Row(){
-                    Button(onClick = { /*TODO*/ }) {
+                    Button(onClick = { /*TODO*/ },
+                        border = BorderStroke(1.dp, Color.Red),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults
+                            .buttonColors(Color.Gray)
+                    ) {
                         Text(text = stringResource(id = R.string.send))
                     }
                     Button(onClick = { /*TODO*/ }) {
                         Text(text = stringResource(id = R.string.receive))
                     }
                 }
-
 
             }
 
