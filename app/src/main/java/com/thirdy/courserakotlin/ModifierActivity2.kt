ff4a588713cf8a87446123cd001e20d5b95ee2e6
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-05-30 11:38:32 +0800

    add ModifierActivity2.kt
    Modifier With Layout
    How to use modifiers to change colors font sizes and edit element

---
+++ app/src/main/java/com/thirdy/courserakotlin/ModifierActivity2.kt
@@ -0,0 +1,94 @@
+package com.thirdy.courserakotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class ModifierActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainComponent2()
+        }
+    }
+}
+
+@Composable
+fun MainComponent2() {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0XFF495E57))
+        ) {
+        Text(
+            text = "Thirdy Graphics",
+            fontSize = 32.sp,
+            color = Color(0xFFF4CE14),
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
+
+        )
+        Text(
+            text ="Pasig",
+            fontSize = 32.sp,
+            color = Color(0xFFF4CE14),
+            modifier = Modifier.padding(start = 20.dp)
+        )
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.Start,
+            ) {
+            Text(
+                text = stringResource(id = R.string.descriptionOne),
+                Modifier.width(200.dp),
+                color = Color.White,
+                fontSize = 21.sp
+            )
+            Image(painter = painterResource(id = R.drawable.prod),
+                contentDescription = "tagayta",
+                Modifier.height(200.dp).clip(RoundedCornerShape(20.dp))
+                )
+        }
+
+        Button(onClick = { /*TODO*/ },
+            border = BorderStroke(1.dp, Color.Red),
+            modifier = Modifier.padding(horizontal = 20.dp),
+            colors = ButtonDefaults.buttonColors(Color(0XFFF4CE14))
+        ) {
+            Text(text = stringResource(R.string.Order))
+        }
+
+    }
+
+}
+
+
+
